70de8bff6c21d38e6dc70b2eee45ac411df76c77
leonw774 <leonw774@gmail.com>
2022-03-18 00:00:42 +0800

    format code to follow PEP8

--- algo.py
+++ algo.py
@@ -1,45 +1,43 @@
-import networkx as nx
 import os
 import subprocess
+import networkx as nx
 
-"""
-    input: transformed virtual network, runingTimeThreshold (in second)
-    return: found path
-"""
-def findTransformedVirtualPath(tfvrNet: nx.Graph, budget: float, runingTimeThreshold: float, noBuildJava: bool):
+
+def find_tfvrpath(tfvrnet: nx.Graph, budget: float, running_time_threshold: float, no_build_java: bool):
+    """
+        input:
+        - transformed virtual network
+        - budget (cost limit)
+        - runing time threshold (in second)
+        - no_build_java: if True, will no build java classes before try to run
+        return: found path
+    """
     ######## make input files
-    query = open('GreedLS/Graph/query.txt', 'w+', encoding='utf8')
-    nodes = open('GreedLS/Graph/Nodes.csv', 'w+', encoding='utf8')
-    arcs = open('GreedLS/Graph/Arcs.txt', 'w+', encoding='utf8')
-    # clear output.txt
-    output = open('GreedLS/Graph/output.txt', 'w+', encoding='utf8')
-    output.close()
+    with open('GreedLS/Graph/query.txt', 'w+', encoding='utf8') as query_file:
+        query_file.write(f'0,0,{budget},{running_time_threshold}\n')
 
-    query.write(f'0,0,{budget},{runingTimeThreshold}\n')
-    query.close()
+    with open('GreedLS/Graph/Nodes.csv', 'w+', encoding='utf8') as node_file:
+        id2node = dict()
+        nodes_str = ''
+        for n in tfvrnet.nodes():
+            nid = tfvrnet.nodes[n]['nid'] = len(id2node)
+            id2node[nid] = n
+            nodes_str += f'n{nid},{n[0]},{n[1]}\n'
+        node_file.write(nodes_str)
 
-    id2node = dict()
-    nodesStr = ''
-    for n in tfvrNet.nodes():
-        id = tfvrNet.nodes[n]['nid'] = len(id2node)
-        id2node[id] = n
-        nodesStr += f'n{id},{n[0]},{n[1]}\n'
-    nodes.write(nodesStr)
-    nodes.close()
-
-    arcsStr = ''
-    for u, v in tfvrNet.edges():
-        # because they only suppport integer cost and value
-        int_weight = int(tfvrNet.edges[u, v]["weight"]*1e3)
-        # print(tfvrNet.edges[u, v]["weight"], int_weight)
-        # because they use directed graph
-        arcsStr += f'{tfvrNet.nodes[u]["nid"]},{tfvrNet.nodes[v]["nid"]}:{int_weight},2\n'
-        arcsStr += f'{tfvrNet.nodes[v]["nid"]},{tfvrNet.nodes[u]["nid"]}:{int_weight},2\n'
-    arcs.write(arcsStr)
-    arcs.close()
+    with open('GreedLS/Graph/Arcs.txt', 'w+', encoding='utf8') as arc_file:
+        arcs_str = ''
+        for u, v in tfvrnet.edges():
+            # because they only suppport integer cost and value
+            int_weight = int(tfvrnet.edges[u, v]["weight"]*1e3)
+            # print(tfvrnet.edges[u, v]["weight"], int_weight)
+            # because they use directed graph
+            arcs_str += f'{tfvrnet.nodes[u]["nid"]},{tfvrnet.nodes[v]["nid"]}:{int_weight},2\n'
+            arcs_str += f'{tfvrnet.nodes[v]["nid"]},{tfvrnet.nodes[u]["nid"]}:{int_weight},2\n'
+        arc_file.write(arcs_str)
 
     ######## build java
-    if not noBuildJava:
+    if not no_build_java:
         os.chdir('./GreedLS')
         subprocess.Popen('javac -d classes src/greedLS/*.java').wait()
         os.chdir('..')
@@ -50,10 +48,12 @@ def findTransformedVirtualPath(tfvrNet: nx.Graph, budget: float, runingTimeThres
     os.chdir("..")
 
     ######## get output
-    output = open('GreedLS/Graph/output.txt', 'r', encoding='utf8')
-    pathStr = output.readlines()[-1]
-    if pathStr[0] == '#':
-        print('findTransformedVirtualPath: cannot find path')
-        exit()
-    path = [id2node.get(int(x), id2node[0]) for x in pathStr[1:-2].split(', ')]
+    with open('GreedLS/Graph/output.txt', 'w+', encoding='utf8') as output_file:
+        path_str = output_file.readlines()[-1]
+        try:
+            assert path_str[0] != '#'
+        except AssertionError as e:
+            print('GreedLS cannot find path')
+            raise e
+    path = [id2node.get(int(x), id2node[0]) for x in path_str[1:-2].split(', ')]
     return path
